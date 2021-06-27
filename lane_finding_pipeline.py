2af09cb9cb36c8901099fe4bf83ee4744be01463
junjiel <jemylei@gmail.com>
2021-06-27 16:54:39 +0200

    add draw line function

--- lane_finding_pipeline.py
+++ lane_finding_pipeline.py
@@ -177,7 +177,7 @@ def fit_polynomial(binary_warped):
     plt.plot(left_fitx, ploty, color='yellow')
     plt.plot(right_fitx, ploty, color='yellow')
 
-    return out_img, left_fit, right_fit, ploty
+    return out_img, left_fit, right_fit, left_fitx, right_fitx, ploty
 
 def fit_poly(img_shape, leftx, lefty, rightx, righty):
     ### TO-DO: Fit a second order polynomial to each with np.polyfit() ###
@@ -287,6 +287,26 @@ def position_from_center(ploty, left_fit, right_fit):
     position = np.abs((rightx-leftx)/2 - image_width/2)*xm_per_pix
     return position
 
+def draw_line(warped, left_fitx, right_fitx, ploty, Minv):
+    # Create an image to draw the lines on
+    warp_zero = np.zeros_like(warped).astype(np.uint8)
+    color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+
+    # Recast the x and y points into usable format for cv2.fillPoly()
+    pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+    pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+    pts = np.hstack((pts_left, pts_right))
+
+    # Draw the lane onto the warped blank image
+    cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
+
+    # Warp the blank back to original image space using inverse perspective matrix (Minv)
+    newwarp = cv2.warpPerspective(color_warp, Minv, (warped.shape[1], warped.shape[0])) 
+    # Combine the result with the original image
+    result = cv2.addWeighted(undist, 1, newwarp, 0.3, 0)
+    return result
+
+
 ##the code below is the pipeline 
 
 # Load the camera calibration specs calculated from CameraCal.py
@@ -340,7 +360,7 @@ fig_binary.savefig('output_images/binary.png')
 
 #Detect lane pixels and fit to find the lane boundary.
 leftx, lefty, rightx, righty, out_img = find_lane_pixels(warped_binary)
-out_img, left_fit, right_fit, ploty = fit_polynomial(warped_binary)
+out_img, left_fit, right_fit, left_fitx, right_fitx, ploty= fit_polynomial(warped_binary)
 #calculate the curvature 
 left_curverad, right_curverad = measure_curvature_pixels(ploty, left_fit, right_fit)
 print('left curvature is',left_curverad,'/n','right curvature is ', right_curverad)
@@ -348,6 +368,11 @@ position = position_from_center(ploty, left_fit, right_fit)
 print('position is ', position)
 plt.figure(figsize=(20,10))
 plt.imshow(out_img)
+
+drwalines = draw_line(warped_binary,left_fitx,right_fitx,ploty,Minv)
+plt.figure()
+plt.imshow(drwalines)
+
 plt.show()
 #plt.close()
 
